c66dfc2c1b0374cc2ffeb37e2791369b6987b04d
Dame <dame.ivanovski@gmail.com>
2020-07-03 17:41:56 +0200

    Logo, Components and fuck ups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,43 @@ import {HttpClientModule} from '@angular/common/http';
 import { SafePipe } from './safe.pipe';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExcerciseComponent } from './excercise/excercise.component';
+import { MainComponent } from './main/main.component';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+import {AngularFireModule} from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyDbxpwqf99flpEtEtHGPIaKBG3e-21rFx4",
+  authDomain: "nickytproj.firebaseapp.com",
+  databaseURL: "https://nickytproj.firebaseio.com",
+  projectId: "nickytproj",
+  storageBucket: "nickytproj.appspot.com",
+  messagingSenderId: "544749017308",
+  appId: "1:544749017308:web:e032e7289dfabc179a3c19"
+}
 
 const routes=[
   {
     path:'',
     component:IntroComponent
+  },
+  {
+    path:'main',
+    children:[
+      {
+        path:'',
+        component:MainComponent
+      },
+      {
+        path:'excercise',
+        component:ExcerciseComponent
+      }
+    ]
   },
   {
     path:'**',
@@ -26,7 +58,11 @@ const routes=[
     AppComponent,
     IntroComponent,
     ErrorComponent,
-    SafePipe
+    SafePipe,
+    ExcerciseComponent,
+    MainComponent,
+    LoginComponent,
+    SignupComponent
     
   ],
   imports: [
@@ -34,7 +70,10 @@ const routes=[
     RouterModule.forRoot(routes),
     HttpClientModule,
     MatExpansionModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    ColorPickerModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
